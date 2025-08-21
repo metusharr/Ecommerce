433bb488d634feaadd2080690bc0fe535ee75e47
metusharr <tusharchoudhary7070@gmail.com>
2025-08-21 11:34:43 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import ProductDetails from "./pages/ProductDetails";
 import NotFound from "./pages/NotFound";
-import HomePage from "./pages/homepage";
 import Header from "./components/Header";
 import Wishlist from "./pages/WishlistPage.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
 import Footer from "./components/footer";
 import Modal from "./pages/modal.jsx";
 import CategoryPage from "./pages/CategoryPage.jsx";
+import HomePage from "./pages/homepage.jsx";
 
 export default function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage/>} />
         <Route path="/category/:categoryId" element={<CategoryPage />} />
         <Route path="/product/:productId" element={<ProductDetails />} />
         <Route path="/wishlist" element={<Wishlist />} />
