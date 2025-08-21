5e954b1cbc27d7af50a3ce32bc5038fdcff954a3
metusharr <tusharchoudhary7070@gmail.com>
2025-08-21 11:46:17 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductDetails from "./pages/ProductDetails";
 import NotFound from "./pages/NotFound";
 import Header from "./components/Header";
-import Wishlist from "./pages/WishlistPage.jsx";
-import ProfilePage from "./pages/ProfilePage.jsx";
 import Footer from "./components/footer";
 import Modal from "./pages/modal.jsx";
 import CategoryPage from "./pages/CategoryPage.jsx";
 import HomePage from "./pages/homepage.jsx";
+import Wishlist from "./pages/WishlistPage.jsx";
+import ProfileDetails from "./pages/ProfilePage.jsx";
 
 export default function App() {
   return (
@@ -20,7 +20,7 @@ export default function App() {
         <Route path="/product/:productId" element={<ProductDetails />} />
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="*" element={<NotFound />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={<ProfileDetails />} />
       </Routes>
       <Footer/>
       <Modal/>
