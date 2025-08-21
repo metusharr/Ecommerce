b6af123ad8fafba0d9e33c333466ae8af5d368c2
metusharr <tusharchoudhary7070@gmail.com>
2025-08-21 12:22:05 +0530

    tt

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import Header from "./components/Header";
 import Footer from "./components/footer";
 import Modal from "./pages/modal.jsx";
 import CategoryPage from "./pages/CategoryPage.jsx";
-import ProfileDetails from "./pages/ProfilePage.jsx";
 import Wishlist from "./pages/WishlistPage.jsx";
 import HomePage from "./pages/homepage.jsx";
+import ProfileDetails from "./pages/ProfileDetails.jsx";
 
 export default function App() {
   return (
