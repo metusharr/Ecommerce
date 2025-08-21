e4bd71c190e595da4e9972705638deee4095eef9
metusharr <tusharchoudhary7070@gmail.com>
2025-08-21 12:20:13 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,16 @@ import Header from "./components/Header";
 import Footer from "./components/footer";
 import Modal from "./pages/modal.jsx";
 import CategoryPage from "./pages/CategoryPage.jsx";
-import HomePage from "./pages/homepage.jsx";
 import ProfileDetails from "./pages/ProfilePage.jsx";
 import Wishlist from "./pages/WishlistPage.jsx";
+import HomePage from "./pages/homepage.jsx";
+
 export default function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage/>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/category/:categoryId" element={<CategoryPage />} />
         <Route path="/product/:productId" element={<ProductDetails />} />
         <Route path="/wishlist" element={<Wishlist />} />
